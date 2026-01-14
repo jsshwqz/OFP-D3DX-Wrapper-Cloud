c5159eacdf005a5713340bc655804f4d3307ccc4
行天 <51498950+jsshwqz@users.noreply.github.com>
2026-01-14 12:51:56 +0800

    Fix COM interface inheritance

--- src/d3d8to9/wrapper.cpp
+++ src/d3d8to9/wrapper.cpp
@@ -3,19 +3,27 @@
 #include <initguid.h>
 
 DEFINE_GUID(IID_IDirect3D8, 0x1DD9E8DA, 0x6C27, 0x41C8, 0xA7, 0xE7, 0x76, 0x15, 0xEE, 0xF7, 0x5C, 0x55);
-DEFINE_GUID(IID_IDirect3DDevice8, 0x1B026D48, 0x1159, 0x4E94, 0xA1, 0xB9, 0xD8, 0x9F, 0xE7, 0x7A, 0xE9, 0x50);
-
-interface IDirect3D8;
-interface IDirect3DDevice8;
-
-typedef interface IDirect3D8 IDirect3D8;
-typedef interface IDirect3DDevice8 IDirect3DDevice8;
 
 static LONG g_RefCount = 0;
 
-IDirect3D8* CreateD3D8Wrapper(IDirect3D9* pD3D9);
+interface IDirect3D8 : public IUnknown
+{
+    virtual HRESULT STDMETHODCALLTYPE RegisterSoftwareDevice(void* pInitializeFunction) = 0;
+    virtual UINT STDMETHODCALLTYPE GetAdapterCount(void) = 0;
+    virtual HRESULT STDMETHODCALLTYPE GetAdapterIdentifier(UINT Adapter, DWORD Flags, void* pIdentifier) = 0;
+    virtual UINT STDMETHODCALLTYPE GetAdapterModeCount(UINT Adapter) = 0;
+    virtual HRESULT STDMETHODCALLTYPE EnumAdapterModes(UINT Adapter, UINT Mode, void* pMode) = 0;
+    virtual HRESULT STDMETHODCALLTYPE GetAdapterDisplayMode(UINT Adapter, void* pMode) = 0;
+    virtual HRESULT STDMETHODCALLTYPE CheckDeviceType(UINT Adapter, D3DDEVTYPE CheckType, D3DFORMAT DisplayFormat, D3DFORMAT BackBufferFormat, BOOL Windowed) = 0;
+    virtual HRESULT STDMETHODCALLTYPE CheckDeviceFormat(UINT Adapter, D3DDEVTYPE DeviceType, D3DFORMAT AdapterFormat, DWORD Usage, D3DRESOURCETYPE RType, D3DFORMAT CheckFormat) = 0;
+    virtual HRESULT STDMETHODCALLTYPE CheckDeviceMultiSampleType(UINT Adapter, D3DDEVTYPE DeviceType, D3DFORMAT SurfaceFormat, BOOL Windowed, D3DMULTISAMPLE_TYPE MultiSampleType) = 0;
+    virtual HRESULT STDMETHODCALLTYPE CheckDepthStencilMatch(UINT Adapter, D3DDEVTYPE DeviceType, D3DFORMAT AdapterFormat, D3DFORMAT RenderTargetFormat, D3DFORMAT DepthStencilFormat) = 0;
+    virtual HRESULT STDMETHODCALLTYPE GetDeviceCaps(UINT Adapter, D3DDEVTYPE DeviceType, void* pCaps) = 0;
+    virtual HMONITOR STDMETHODCALLTYPE GetAdapterMonitor(UINT Adapter) = 0;
+    virtual HRESULT STDMETHODCALLTYPE CreateDevice(UINT Adapter, D3DDEVTYPE DeviceType, HWND hFocusWindow, DWORD BehaviorFlags, void* pPresentationParameters, void** ppReturnedDeviceInterface) = 0;
+};
 
-class CDirect3D8 : public IUnknown
+class CDirect3D8 : public IDirect3D8
 {
 private:
     IDirect3D9* m_pD3D9;
@@ -60,92 +68,81 @@ public:
         return RefCount;
     }
 
-    STDMETHOD(RegisterSoftwareDevice)(THIS_ void* pInitializeFunction)
+    STDMETHOD(RegisterSoftwareDevice)(THIS_ void* pInitializeFunction) override
     {
         return m_pD3D9->RegisterSoftwareDevice(pInitializeFunction);
     }
 
-    STDMETHOD_(UINT, GetAdapterCount)(THIS)
+    STDMETHOD_(UINT, GetAdapterCount)(THIS) override
     {
         return m_pD3D9->GetAdapterCount();
     }
 
-    STDMETHOD(GetAdapterIdentifier)(THIS_ UINT Adapter, DWORD Flags, D3DADAPTER_IDENTIFIER9* pIdentifier)
+    STDMETHOD(GetAdapterIdentifier)(THIS_ UINT Adapter, DWORD Flags, void* pIdentifier) override
     {
-        return m_pD3D9->GetAdapterIdentifier(Adapter, Flags, pIdentifier);
+        return m_pD3D9->GetAdapterIdentifier(Adapter, Flags, (D3DADAPTER_IDENTIFIER9*)pIdentifier);
     }
 
-    STDMETHOD_(UINT, GetAdapterModeCount)(THIS_ UINT Adapter)
+    STDMETHOD_(UINT, GetAdapterModeCount)(THIS_ UINT Adapter) override
     {
         return m_pD3D9->GetAdapterModeCount(Adapter, D3DFMT_UNKNOWN);
     }
 
-    STDMETHOD(EnumAdapterModes)(THIS_ UINT Adapter, UINT Mode, D3DDISPLAYMODE* pMode)
+    STDMETHOD(EnumAdapterModes)(THIS_ UINT Adapter, UINT Mode, void* pMode) override
     {
-        return m_pD3D9->EnumAdapterModes(Adapter, D3DFMT_UNKNOWN, Mode, pMode);
+        return m_pD3D9->EnumAdapterModes(Adapter, D3DFMT_UNKNOWN, Mode, (D3DDISPLAYMODE*)pMode);
     }
 
-    STDMETHOD(GetAdapterDisplayMode)(THIS_ UINT Adapter, D3DDISPLAYMODE* pMode)
+    STDMETHOD(GetAdapterDisplayMode)(THIS_ UINT Adapter, void* pMode) override
     {
-        return m_pD3D9->GetAdapterDisplayMode(Adapter, pMode);
+        return m_pD3D9->GetAdapterDisplayMode(Adapter, (D3DDISPLAYMODE*)pMode);
     }
 
-    STDMETHOD(CheckDeviceType)(THIS_ UINT Adapter, D3DDEVTYPE CheckType, D3DFORMAT DisplayFormat, D3DFORMAT BackBufferFormat, BOOL Windowed)
+    STDMETHOD(CheckDeviceType)(THIS_ UINT Adapter, D3DDEVTYPE CheckType, D3DFORMAT DisplayFormat, D3DFORMAT BackBufferFormat, BOOL Windowed) override
     {
         return m_pD3D9->CheckDeviceType(Adapter, CheckType, DisplayFormat, BackBufferFormat, Windowed);
     }
 
-    STDMETHOD(CheckDeviceFormat)(THIS_ UINT Adapter, D3DDEVTYPE DeviceType, D3DFORMAT AdapterFormat, DWORD Usage, D3DRESOURCETYPE RType, D3DFORMAT CheckFormat)
+    STDMETHOD(CheckDeviceFormat)(THIS_ UINT Adapter, D3DDEVTYPE DeviceType, D3DFORMAT AdapterFormat, DWORD Usage, D3DRESOURCETYPE RType, D3DFORMAT CheckFormat) override
     {
         return m_pD3D9->CheckDeviceFormat(Adapter, DeviceType, AdapterFormat, Usage, RType, CheckFormat);
     }
 
-    STDMETHOD(CheckDeviceMultiSampleType)(THIS_ UINT Adapter, D3DDEVTYPE DeviceType, D3DFORMAT SurfaceFormat, BOOL Windowed, D3DMULTISAMPLE_TYPE MultiSampleType)
+    STDMETHOD(CheckDeviceMultiSampleType)(THIS_ UINT Adapter, D3DDEVTYPE DeviceType, D3DFORMAT SurfaceFormat, BOOL Windowed, D3DMULTISAMPLE_TYPE MultiSampleType) override
     {
         return m_pD3D9->CheckDeviceMultiSampleType(Adapter, DeviceType, SurfaceFormat, Windowed, MultiSampleType, nullptr);
     }
 
-    STDMETHOD(CheckDepthStencilMatch)(THIS_ UINT Adapter, D3DDEVTYPE DeviceType, D3DFORMAT AdapterFormat, D3DFORMAT RenderTargetFormat, D3DFORMAT DepthStencilFormat)
+    STDMETHOD(CheckDepthStencilMatch)(THIS_ UINT Adapter, D3DDEVTYPE DeviceType, D3DFORMAT AdapterFormat, D3DFORMAT RenderTargetFormat, D3DFORMAT DepthStencilFormat) override
     {
         return m_pD3D9->CheckDepthStencilMatch(Adapter, DeviceType, AdapterFormat, RenderTargetFormat, DepthStencilFormat);
     }
 
-    STDMETHOD(GetDeviceCaps)(THIS_ UINT Adapter, D3DDEVTYPE DeviceType, void* pCaps)
+    STDMETHOD(GetDeviceCaps)(THIS_ UINT Adapter, D3DDEVTYPE DeviceType, void* pCaps) override
     {
         D3DCAPS9 caps9;
         return m_pD3D9->GetDeviceCaps(Adapter, DeviceType, &caps9);
     }
 
-    STDMETHOD_(HMONITOR, GetAdapterMonitor)(THIS_ UINT Adapter)
+    STDMETHOD_(HMONITOR, GetAdapterMonitor)(THIS_ UINT Adapter) override
     {
         return m_pD3D9->GetAdapterMonitor(Adapter);
     }
 
-    STDMETHOD(CreateDevice)(THIS_ UINT Adapter, D3DDEVTYPE DeviceType, HWND hFocusWindow, DWORD BehaviorFlags, void* pPresentationParameters, void** ppReturnedDeviceInterface);
+    STDMETHOD(CreateDevice)(THIS_ UINT Adapter, D3DDEVTYPE DeviceType, HWND hFocusWindow, DWORD BehaviorFlags, void* pPresentationParameters, void** ppReturnedDeviceInterface) override
+    {
+        if (ppReturnedDeviceInterface == nullptr)
+            return D3DERR_INVALIDCALL;
+
+        D3DPRESENT_PARAMETERS pp9 = {0};
+        IDirect3DDevice9* pDevice9 = nullptr;
+        return m_pD3D9->CreateDevice(Adapter, DeviceType, hFocusWindow, BehaviorFlags, &pp9, &pDevice9);
+    }
 };
 
-IDirect3D8* CreateD3D8Wrapper(IDirect3D9* pD3D9)
+extern "C" __declspec(dllexport) IDirect3D8* CreateD3D8Wrapper(IDirect3D9* pD3D9)
 {
     if (pD3D9 == nullptr)
         return nullptr;
     return new CDirect3D8(pD3D9);
-}
-
-STDMETHODIMP CDirect3D8::CreateDevice(THIS_ UINT Adapter, D3DDEVTYPE DeviceType, HWND hFocusWindow, DWORD BehaviorFlags, void* pPresentationParameters, void** ppReturnedDeviceInterface)
-{
-    if (ppReturnedDeviceInterface == nullptr)
-        return D3DERR_INVALIDCALL;
-
-    D3DPRESENT_PARAMETERS pp9;
-    memset(&pp9, 0, sizeof(pp9));
-
-    IDirect3DDevice9* pDevice9 = nullptr;
-    HRESULT hr = m_pD3D9->CreateDevice(Adapter, DeviceType, hFocusWindow, BehaviorFlags, &pp9, &pDevice9);
-    
-    if (SUCCEEDED(hr) && pDevice9)
-    {
-        *ppReturnedDeviceInterface = nullptr;
-        pDevice9->Release();
-    }
-    return hr;
 }
